9681abf298f6b5c7ed853c1f8fdb8f7b476ba73b
dianaboikoo <eaa23dibo@students.eaaa.dk>
2023-12-18 23:29:25 +0100

    yees

---
+++ js/wildhorses.js
@@ -0,0 +1,18 @@
+var currentImageIndex = 1; // Starts with the first image
+
+function changeImage(direction) {
+  var pic = document.getElementById('pic');
+
+  // Updates the image index based on the direction
+  currentImageIndex += direction;
+
+  // Checks the index
+  if (currentImageIndex < 1) {
+    currentImageIndex = 4; 
+  } else if (currentImageIndex > 4) {
+    currentImageIndex = 1;
+  }
+
+  // Updates the image source with new index
+  pic.src = 'Images/horses' + currentImageIndex + '.png';
+}
